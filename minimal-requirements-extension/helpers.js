7ceb48a366f330650c4e16b1063b596666fcb71c
Sandro Camastral <sandro.camastral@sap.com>
2022-08-08 10:50:49 +0200

    add minimal requirements extension

---
+++ minimal-requirements-extension/helpers.js
@@ -0,0 +1,19 @@
+//
+// Loop before a token expire to fetch a new one
+//
+function initializeRefreshTokenStrategy(shellSdk, auth) {
+
+  shellSdk.on(SHELL_EVENTS.Version1.REQUIRE_AUTHENTICATION, (event) => {
+    sessionStorage.setItem('token', event.access_token);
+    setTimeout(() => fetchToken(), (event.expires_in * 1000) - 5000);
+  });
+
+  function fetchToken() {
+    shellSdk.emit(SHELL_EVENTS.Version1.REQUIRE_AUTHENTICATION, {
+      response_type: 'token'  // request a user token within the context
+    });
+  }
+
+  sessionStorage.setItem('token', auth.access_token);
+  setTimeout(() => fetchToken(), (auth.expires_in * 1000) - 5000);
+}
